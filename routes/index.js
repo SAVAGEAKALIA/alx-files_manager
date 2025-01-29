60dd675482354d10f6a440bdab0d50d3860085f7
SAVAGEAKALIA <akaliasaviour@gmail.com>
2025-01-29 14:35:21 +0100

    File_Manager Done 2

--- routes/index.js
+++ routes/index.js
@@ -16,8 +16,8 @@ router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
 router.post('/files', FilesController.postUpload);
 router.get('/files', FilesController.getIndex);
-router.get('/files/:id', FilesController.getShow)
-router.put('/files/:id/publish', FilesController.putPublish)
-router.put('/files/:id/unpublish', FilesController.putUnpublish)
-router.get('/files/:id/data', FilesController.getFile)
+router.get('/files/:id', FilesController.getShow);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
 export default router
