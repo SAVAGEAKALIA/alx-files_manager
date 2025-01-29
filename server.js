1df1f794978a41a751e4b30aa6f97beab32a68d8
SAVAGEAKALIA <akaliasaviour@gmail.com>
2025-01-29 13:10:13 +0100

    File_Manager Done

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 /* eslint-disable */
 // server.js
 import express from 'express';
-import routes from './routes/index';
+//Changed routes to router to check if error from here
+import router from './routes/index';
 import process from 'process';
 import fileQueue from './worker';
 import redisClient from './utils/redis';
@@ -14,7 +15,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // Routes
-app.use('/', routes);
+app.use('/', router);
 
 // Start server - modified to listen on all interfaces
 app.listen(port,() => {
