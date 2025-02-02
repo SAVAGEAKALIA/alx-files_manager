4ff3ff8ef56fde87e81e2f44333f5b72ad204b9c
SAVAGEAKALIA <akaliasaviour@gmail.com>
2025-02-02 08:13:21 +0100

    File_Manager Done 4

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -47,31 +47,32 @@ class UsersController {
     }
 
     static async getMe(req, res) {
-        // 1. get X-token header
-        // console.log('Headers:', req.headers);console.log('Headers:', req.headers);
-        const authorization_header_token = req.headers['x-token'] || '';
+        try {
+            const authorization_header_token = req.headers['x-token'] || '';
 
-        // console.log(authorization_header_token)
+            if (!authorization_header_token || authorization_header_token.trim() === '') {
+                return res.status(401).json({ error: 'Unauthorized' });
+            }
 
-        if (!authorization_header_token) {
-            return res.status(401).json({ error: 'Missing or invalid authorization token user control'});
+            const user_id = await redisClient.get(`auth_${authorization_header_token}`);
+
+            if (!user_id) {
+                return res.status(401).json({ error: 'Unauthorized' });
+            }
+
+            const user_logged = await dbClient.db.collection('users').findOne({ _id: ObjectId(user_id) });
+
+            if (!user_logged) {
+                return res.status(404).json({ error: 'User not found' });
+            }
+
+            return res.status(200).json({ id: user_logged._id, email: user_logged.email });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ error: 'Server error' });
         }
-        // 2. split and decode token
-        // const tokenId = authorization_header_token
-
-        // 3. delete Redis key
-        const user_id = await redisClient.get(`auth_${authorization_header_token}`);
-
-        // console.log(user_id);
-
-        if (!user_id) {
-            return res.status(401).json({ error: 'Unauthorized' });
-        }
-        const user_logged = await dbClient.db.collection('users').findOne({ _id: ObjectId(user_id) })
-        // console.log(user_logged)
-
-        return res.status(201).json({id: user_logged._id, email: user_logged.email});
     }
+
 }
 
 export default UsersController;
