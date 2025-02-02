da420bef2e3b989a3eef5fdf65c807558944e0c6
SAVAGEAKALIA <akaliasaviour@gmail.com>
2025-02-02 07:51:07 +0100

    File_Manager Done 3

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -13,8 +13,12 @@ class AuthController{
         // 1. get authorization header
         const authorization_header = req.headers.authorization || ""
 
+        if (!authorization_header) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
         if (!authorization_header.startsWith('Basic ')) {
-            return res.status(401).json({ error: 'Missing or invalid authorization token' });
+            return res.status(401).json({ error: 'Unauthorized' });
         }
 
         // 3. split and decode authorization header
@@ -26,18 +30,22 @@ class AuthController{
         // console.log('Email:', email); // Debug log
         // console.log('Password:', password); // Debug log
         if (!email) {
-            return res.status(401).json({ error: 'Invalid email' });
+            return res.status(401).json({  error: 'Unauthorized' });
         }
 
         if (!password) {
-            return res.status(401).json({ error: 'Invalid password' });
+            return res.status(401).json({  error: 'Unauthorized' });
         }
 
         // 3. hash password
         const hashPassword = crypto.createHash('sha1').update(password).digest('hex');
-        const user = await dbClient.db.collection('users').findOne({ email, password: hashPassword})
+        const user = await dbClient.db.collection('users').findOne({ email })
 
         if (!user) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        if (user.password !== hashPassword) {
             return res.status(401).json({ error: 'Unauthorized' });
         }
 
@@ -72,33 +80,6 @@ class AuthController{
         await redisClient.del(`auth_${token}`);
 
         return res.status(204).send();
-    }
-
-    static async getMe(req, res) {
-        // 1. get X-token header
-        // console.log('Headers:', req.headers);console.log('Headers:', req.headers);
-        const authorization_header_token = req.headers['x-token'] || '';
-
-        // console.log(authorization_header_token)
-
-        if (!authorization_header_token) {
-            return res.status(401).json({ error: 'Missing or invalid authorization token user control'});
-        }
-        // 2. split and decode token
-        // const tokenId = authorization_header_token
-
-        // 3. delete Redis key
-        const user_id = await redisClient.get(`auth_${authorization_header_token}`);
-
-        // console.log(user_id);
-
-        if (!user_id) {
-            return res.status(401).json({ error: 'Unauthorized' });
-        }
-        const user_logged = await dbClient.db.collection('users').findOne({ _id: ObjectId(user_id) })
-        // console.log(user_logged)
-
-        return res.status(201).json({id: user_logged._id, email: user_logged.email});
     }
 }
 
